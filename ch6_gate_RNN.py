fa3c7e5a21690b839583d00529427cfafd750b5a
yeonggeumkang <realyoungm@naver.com>
2019-11-27 23:09:13 +0900

    Create class TimeSTM
    미완성

--- ch6_gate_RNN.py
+++ ch6_gate_RNN.py
@@ -68,6 +68,7 @@ class LSTM:
         self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
         return h_next, c_next
 
+# TimeSTM class 구현
 class TimeSTM:
     def __init__(self, Wx, Wh, b, stateful=False):
         self.params = [Wx, Wh, b]
